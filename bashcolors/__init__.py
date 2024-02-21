5b9ea889cd891c880c362cbcaa001270109a6cea
TheeMichael <bryan.u@comcast.net>
2024-02-21 13:19:19 -0600

    init docstring

--- bashcolors/__init__.py
+++ bashcolors/__init__.py
@@ -1,1 +1,8 @@
+r"""
+Bash Colors provides f-string usable constants for color coding terminal outputs using bash
+
+Example:
+  - import bash_colors as bc
+  - print(f"{bc.GREEN_B}Example Text!{bc.ENDC}")
+"""
 from .bashcolors import *
